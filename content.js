93a0cdff8956e3c0d65ca8c458999c307be2c902
deepsidh9 <deep.barca@gmail.com>
2020-04-24 06:07:10 +0530

    Added advancing to next episode functionality

--- content.js
+++ content.js
@@ -3,19 +3,33 @@ function getElementbyXpath(xpath) {
     return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null).iterateNext();
 }
 
-function clickOnSkip() {
+function clickOnSkipAndNext() {
     var skipButtonClassName = 'nf-icon-button nf-flat-button nf-flat-button-uppercase no-icon'
     var skipButton = document.getElementsByClassName(skipButtonClassName)
 
+    var nextEpisodeClassName = '.draining'
+    var nextEpisodeButton = document.querySelector(nextEpisodeClassName)
+
     console.log("skipButton", skipButton)
     console.log("length of array as", skipButton.length)
+
+
     if (skipButton.length > 0) {
-        console.log("Element Visible")
+        console.log("Skip Button Element Visible")
         console.log("Clicking")
         skipButton[0].click();
+        setTimeout(function () { clickOnSkipAndNext() }, 1000); // wait for 1 second to check the skip intro button
+    }
+    else if (nextEpisodeButton) {
+        console.log("Next Episode Button Element Visible")
+        console.log("Pressing Next")
+        const pressEnterEvent = new KeyboardEvent("keydown", {
+            bubbles: true, cancelable: true, keyCode: 13
+        });
+        nextEpisodeButton.dispatchEvent(pressEnterEvent);
     }
     else {
-        setTimeout(function () { clickOnSkip() }, 1000); // wait for 1 second to check the skip intro button
+        setTimeout(function () { clickOnSkipAndNext() }, 1000); // wait for 1 second to check the skip intro button
     }
 }
 
@@ -27,15 +41,7 @@ chrome.runtime.onMessage.addListener(
         console.log("Content.js logging : Message Received", request.message)
 
         if (request.message === "click_on_skip") {
-            clickOnSkip();
+            clickOnSkipAndNext();
         }
     }
 );
-
-// var skipButton = getElementsByClassName('//*[@id="appMountPoint"]/div/div/div[1]/div/div/div[2]/div/div[3]/div[1]/a/span')
-// var skipButtonXpath = "//a[contains(text(),'Skip Intro')]";
-// var skipButtonXpath = "//a[text()='Skip Intro']";
-
-// var skipButton = getElementbyXpath(skipButtonXpath)
-
-// var skipButton = document.getElementsByClassName('nf-flat-button-text')
